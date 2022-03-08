b308f11c64861e7d634799612b74c8076ba99c2a
Miyka96 <micaelaseminara@outlook.it>
2022-03-08 19:58:48 +0100

    VueHeader component

--- src/App.vue
+++ src/App.vue
@@ -27,5 +27,22 @@ export default {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+.container-fluid{
+  width: 100%;
+  min-height: 100px;
+}
+
+.container{
+  width: 1200px;
+  margin: 0 auto;
+}
+li{
+  list-style: none;
+}
+a{
+  text-decoration: none;
+  color: currentColor;
 }
 </style>
--- src/components/VueHeader.vue
+++ src/components/VueHeader.vue
@@ -1,19 +1,79 @@
 <template>
-    <div> {{ text }} </div>
+  <div class="container-fluid">
+    <div class="container">
+      <figure>
+        <img src="../assets/img/dc-logo.png" alt="" />
+      </figure>
+      <div class="nav">
+        <ul>
+          <li><a href="#"> list item </a></li>
+          <li><a href="#"> list item </a></li>
+          <li><a href="#"> list item </a></li>
+          <li><a href="#"> list item </a></li>
+          <li><a href="#"> list item </a></li>
+          <li><a href="#"> list item </a></li>
+          <li><a href="#"> list item </a></li>
+          <li><a href="#"> list item </a></li>
+          <li><a href="#"> list item </a></li>
+          <li><a href="#"> list item </a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "VueHeader",
+  name: "VueHeader",
 
-    data() {
-        return{
-        text: "header here",
-        }
-    }
-}
+  data() {
+    return {
+      text: "header here",
+    };
+  },
+};
 </script>
 
-<style lang="sass" scoped>
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  align-items: center;
+  height: 110px;
+}
 
+figure {
+  width: 30%;
+}
+
+.nav {
+  flex-grow: 1;
+  height: 100%;
+}
+
+ul {
+  display: flex;
+  flex-wrap: nowrap;
+  height: 100%;
+  align-items: center;
+  border-bottom: 4px solid white;
+
+  li {
+    padding: 0 12px;
+
+    text-transform: uppercase;
+    font-size: 12px;
+
+    a {
+        color: rgb(61, 61, 61);
+        padding-bottom: 45px;
+        padding-top: 40px;
+        font-weight: 700;
+
+      &:hover {
+        border-bottom: 4px solid blue;
+        color: blue;
+      }
+    }
+  }
+}
 </style>
