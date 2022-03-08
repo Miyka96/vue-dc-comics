16c480a1a02a2e2b88bb06e42fb742a4c2f36bce
Miyka96 <micaelaseminara@outlook.it>
2022-03-08 18:19:50 +0100

    created files

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+     <div> {{ text }} </div>   
+</template>
+
+<script>
+export default {
+    name: Footer,
+
+    data() {
+        return{            
+        text: "main here",
+        }
+    }
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div> {{ text }} </div>
+</template>
+
+<script>
+export default {
+    name: Header,
+
+    data() {
+        return{
+        text: "header here",
+        }
+    }
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
